d9fd51f691ca8245162269db46a9a3d5d9710a4d
amitxai <info.amitkumar42@gmail.com>
2025-12-12 22:49:31 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// 👇 Ye line sabse zaroori hai, isse hi Rang (Colors) aayenge
+import './index.css'; 
 import App from './App';
 
 const rootElement = document.getElementById('root');
