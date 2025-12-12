8beb44597ee943407e4c36eca204b9c77a1c88f6
amitxai <info.amitkumar42@gmail.com>
2025-12-13 03:54:55 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,14 @@ import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, (process as any).cwd(), "");
+  const env = loadEnv(mode, process.cwd(), "");
   return {
     plugins: [react()],
     define: {
-      __APP_ENV__: env.APP_ENV,
+      'process.env': env // Safe env access
     },
+    server: {
+      port: 3000
+    }
   };
 });
