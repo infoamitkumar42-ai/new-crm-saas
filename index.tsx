54494f54b3bbde77bb1f7725e64f3dda380da2d7
amitxai <info.amitkumar42@gmail.com>
2025-12-13 21:51:35 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css'; 
 import App from './App';
-// 👇 Ye line missing thi, isliye screen white thi
+// 👇 Ye line missing thi, isliye screen blank thi
 import { AuthProvider } from './auth/useAuth';
 
 const rootElement = document.getElementById('root');
@@ -13,6 +13,7 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
+    {/* 👇 AuthProvider lagana zaroori hai */}
     <AuthProvider>
       <App />
     </AuthProvider>
