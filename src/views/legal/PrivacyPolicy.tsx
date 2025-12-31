67893672f5f7f48127640f34c7911c5746500c6a
amitxai <info.amitkumar42@gmail.com>
2026-01-01 03:13:45 +0530

    Update PrivacyPolicy.tsx

--- src/views/legal/PrivacyPolicy.tsx
+++ src/views/legal/PrivacyPolicy.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 
-export const PrivacyPolicy = () => {
+const PrivacyPolicy = () => {
   useEffect(() => { window.scrollTo(0, 0); }, []);
 
   return (
@@ -39,3 +39,5 @@ export const PrivacyPolicy = () => {
     </div>
   );
 };
+
+export default PrivacyPolicy;
